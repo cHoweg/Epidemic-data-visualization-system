3f31601890a5c43b208fbb0603f9d801f1f5a203
EugeneChow <574128041@qq.com>
2021-04-15 22:31:50 +0800

    Default Changelist

--- src/main/java/com/zoll/vinfo/controller/DataController.java
+++ src/main/java/com/zoll/vinfo/controller/DataController.java
@@ -8,8 +8,8 @@ import com.zoll.vinfo.service.DataDetailService;
 import com.zoll.vinfo.service.DataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -29,9 +29,10 @@ public class DataController {
 
     //首页显示所有相关数据
     @GetMapping("/")
-    public String list(Model model) {
+    public ModelAndView list() {
+        ModelAndView modelAndView = new ModelAndView();
         List<DataBean> dataList = dataService.list();
-        model.addAttribute("dataList", dataList);
+        modelAndView.addObject("dataList", dataList);
 
         List<MapBean> result = new ArrayList<>();
         for (int i = 0; i < dataList.size(); i++) {
@@ -40,7 +41,7 @@ public class DataController {
             result.add(mapBean);
 
         }
-        model.addAttribute("mapData", new Gson().toJson(result));
+        modelAndView.addObject("mapData", new Gson().toJson(result));
 
         String str = GraphHandler.getData();
         List<GraphBean> list = GraphHandler.getGraphData(str);
@@ -56,8 +57,8 @@ public class DataController {
             nowConfirmList.add(graphBean.getNowConfirm());
         }
 
-        model.addAttribute("dateList", new Gson().toJson(dateList));
-        model.addAttribute("nowConfirmList", new Gson().toJson(nowConfirmList));
+        modelAndView.addObject("dateList", new Gson().toJson(dateList));
+        modelAndView.addObject("nowConfirmList", new Gson().toJson(nowConfirmList));
 
 
         List<GraphAddBean> addList = GraphHandler.getGraphAddData(str);
@@ -73,14 +74,14 @@ public class DataController {
             addSuspectList.add(graphAddBean.getAddSuspect());
         }
 
-        model.addAttribute("addDateList", new Gson().toJson(addDateList));
-        model.addAttribute("addConfirmList", new Gson().toJson(addConfirmList));
-        model.addAttribute("addSuspectList", new Gson().toJson(addSuspectList));
+        modelAndView.addObject("addDateList", new Gson().toJson(addDateList));
+        modelAndView.addObject("addConfirmList", new Gson().toJson(addConfirmList));
+        modelAndView.addObject("addSuspectList", new Gson().toJson(addSuspectList));
 
 
         List<GraphPieBean> pieList = GraphHandler.getGraphPieData(str);
         Collections.sort(pieList);
-        model.addAttribute("pieList", new Gson().toJson(pieList));
+        modelAndView.addObject("pieList", new Gson().toJson(pieList));
 
         List<GraphColumnarBean> columnarList = GraphHandler.getGraphColumnarData();
         Collections.sort(columnarList);
@@ -94,20 +95,21 @@ public class DataController {
             fromAbroadList.add(bean.getFromAbroad());
         }
 
-        model.addAttribute("nameList", new Gson().toJson(nameList));
-        model.addAttribute("fromAbroadList", new Gson().toJson(fromAbroadList));
+        modelAndView.addObject("nameList", new Gson().toJson(nameList));
+        modelAndView.addObject("fromAbroadList", new Gson().toJson(fromAbroadList));
 
-        return "list";
+        return modelAndView;
     }
 
 
     /**
      * 单个折线图显示现存确诊人数
-     * @param model
+     * @param
      * @return
      */
     @GetMapping("/graph")
-    public String graph(Model model) {
+    public ModelAndView graph() {
+        ModelAndView modelAndView = new ModelAndView();
         List<GraphBean> list = GraphHandler.getGraphData();
         //  进一步改造数据格式
         //  因为前端需要的数据是  x轴所有数据的数组和y轴所有数据的数组
@@ -121,18 +123,19 @@ public class DataController {
             nowConfirmList.add(graphBean.getNowConfirm());
         }
 
-        model.addAttribute("dateList", new Gson().toJson(dateList));
-        model.addAttribute("nowConfirmList", new Gson().toJson(nowConfirmList));
-        return "graph";
+        modelAndView.addObject("dateList", new Gson().toJson(dateList));
+        modelAndView.addObject("nowConfirmList", new Gson().toJson(nowConfirmList));
+        return modelAndView;
     }
 
     /**
      * 双重折线图显示新增确诊人数和疑似确诊人数
-     * @param model
+     * @param
      * @return
      */
     @GetMapping("/graphAdd")
-    public String graphAdd(Model model) {
+    public ModelAndView graphAdd() {
+        ModelAndView modelAndView = new ModelAndView();
         List<GraphAddBean> list = GraphHandler.getGraphAddData();
 
         ArrayList<String> dateList = new ArrayList<>();
@@ -146,19 +149,20 @@ public class DataController {
             addSuspectList.add(graphAddBean.getAddSuspect());
         }
 
-        model.addAttribute("dateList", new Gson().toJson(dateList));
-        model.addAttribute("addConfirmList", new Gson().toJson(addConfirmList));
-        model.addAttribute("addSuspectList", new Gson().toJson(addSuspectList));
-        return "graphAdd";
+        modelAndView.addObject("dateList", new Gson().toJson(dateList));
+        modelAndView.addObject("addConfirmList", new Gson().toJson(addConfirmList));
+        modelAndView.addObject("addSuspectList", new Gson().toJson(addSuspectList));
+        return modelAndView;
     }
 
     /**
      * 显示全国排名前十的境外输入人数的条形统计图
-     * @param model
+     * @param
      * @return
      */
     @GetMapping("/graphColumnar")
-    public String graphColumnar(Model model) {
+    public ModelAndView graphColumnar() {
+        ModelAndView modelAndView = new ModelAndView();
         List<GraphColumnarBean> list = GraphHandler.getGraphColumnarData();
         Collections.sort(list);
 
@@ -171,33 +175,35 @@ public class DataController {
             fromAbroadList.add(bean.getFromAbroad());
         }
 
-        model.addAttribute("nameList", new Gson().toJson(nameList));
-        model.addAttribute("fromAbroadList", new Gson().toJson(fromAbroadList));
-        return "graphColumnar";
+        modelAndView.addObject("nameList", new Gson().toJson(nameList));
+        modelAndView.addObject("fromAbroadList", new Gson().toJson(fromAbroadList));
+        return modelAndView;
     }
 
 
     /**
      * 饼状图
-     * @param model
+     * @param
      * @return
      */
     @GetMapping("/graphPie")
-    public String graphPie(Model model) {
+    public ModelAndView graphPie() {
+        ModelAndView modelAndView = new ModelAndView();
         List<GraphPieBean> list = GraphHandler.getGraphPieData();
         Collections.sort(list);
-        model.addAttribute("list", new Gson().toJson(list));
-        return "graphPie";
+        modelAndView.addObject("list", new Gson().toJson(list));
+        return modelAndView;
     }
 
 
     /**
      * 根据地区名和确诊人数绘制中国地图
-     * @param model
+     * @param
      * @return
      */
     @GetMapping("/map")
-    public String map(Model model) {
+    public ModelAndView map() {
+        ModelAndView modelAndView = new ModelAndView();
         List<DataBean> list = dataService.list();
 
         List<MapBean> result = new ArrayList<>();
@@ -207,53 +213,8 @@ public class DataController {
             result.add(mapBean);
 
         }
-        model.addAttribute("mapData", new Gson().toJson(result));
-        return "map";
+        modelAndView.addObject("mapData", new Gson().toJson(result));
+        return modelAndView;
     }
 
-    @GetMapping(value={"/guangDong-NowConfirm"})
-    public String guangDongNowConfirm(Model model) {
-        List<DataBean> list = dataService.list();
-
-        List<MapBean> result = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            DataBean dataBean = list.get(i);
-            MapBean mapBean = new MapBean("佛山市",100);
-            result.add(mapBean);
-        }
-        model.addAttribute("mapData", new Gson().toJson(result));
-        return "guangDong-NowConfirm";
-    }
-
-    @GetMapping("/guangDong-province")
-    public String guangDongMonth(Model model) {
-        List<DataDetailBean> dataList = dataDetailService.list();
-        model.addAttribute("dataList", dataList);
-        List<GraphBean> list = GraphHandler.getGraphData();
-        //  进一步改造数据格式
-        //  因为前端需要的数据是  x轴所有数据的数组和y轴所有数据的数组
-
-        ArrayList<String> dateList = new ArrayList<>();
-        ArrayList<Integer> nowConfirmList = new ArrayList<>();
-
-        for (int i = 0; i < list.size(); i++) {
-            GraphBean graphBean = list.get(i);
-            dateList.add(graphBean.getDate());
-            nowConfirmList.add(graphBean.getNowConfirm());
-        }
-
-        List<MapBean> result = new ArrayList<>();
-        for (int i = 0; i < dataList.size(); i++) {
-            DataDetailBean dataDetailBean = dataList.get(i);
-            MapBean mapBean = new MapBean("address",dataDetailBean.getProvince_id());
-            result.add(mapBean);
-        }
-
-
-
-        model.addAttribute("mapData", new Gson().toJson(result));
-        model.addAttribute("dateList", new Gson().toJson(dateList));
-        model.addAttribute("nowConfirmList", new Gson().toJson(nowConfirmList));
-        return "guangDong-Province";
-    }
 }

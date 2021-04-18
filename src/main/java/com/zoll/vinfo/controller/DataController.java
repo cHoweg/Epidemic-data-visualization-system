a6bbba05bdbc02bbfdffd10bba9fe4c2f7416c83
liuzhanyang9988 <1137660913@qq.com>
2021-04-18 14:28:33 +0800

    Default Changelist

--- src/main/java/com/zoll/vinfo/controller/DataController.java
+++ src/main/java/com/zoll/vinfo/controller/DataController.java
@@ -9,9 +9,11 @@ import com.zoll.vinfo.service.DataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -221,5 +223,79 @@ public class DataController {
         modelAndView.addObject("mapData", new Gson().toJson(result));
         modelAndView.setViewName("map");
         return modelAndView;
+    }
+
+    @RequestMapping(value = "/provincelist")
+    public String pl(Model model,@RequestParam(value = "select2",required = false) String id) {
+//        ModelAndView modelAndView = new ModelAndView();
+        List<DataBean> dataList = dataService.list();
+        model.addAttribute("dataList", dataList);
+
+        List<MapBean> result = new ArrayList<>();
+        for (int i = 0; i < dataList.size(); i++) {
+            DataBean dataBean = dataList.get(i);
+            MapBean mapBean = new MapBean(dataBean.getArea(), dataBean.getNowConfirm());
+            result.add(mapBean);
+
+        }
+        model.addAttribute("mapData", new Gson().toJson(result));
+
+        String str = GraphHandler.getData();
+        List<GraphBean> list = GraphHandler.getGraphData(str);
+        //  进一步改造数据格式
+        //  因为前端需要的数据是  x轴所有数据的数组和y轴所有数据的数组
+
+        ArrayList<String> dateList = new ArrayList<>();
+        ArrayList<Integer> nowConfirmList = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            GraphBean graphBean = list.get(i);
+            dateList.add(graphBean.getDate());
+            nowConfirmList.add(graphBean.getNowConfirm());
+        }
+
+        model.addAttribute("dateList", new Gson().toJson(dateList));
+        model.addAttribute("nowConfirmList", new Gson().toJson(nowConfirmList));
+
+
+        List<GraphAddBean> addList = GraphHandler.getGraphAddData(str);
+
+        ArrayList<String> addDateList = new ArrayList<>();
+        ArrayList<Integer> addConfirmList = new ArrayList<>();
+        ArrayList<Integer> addSuspectList = new ArrayList<>();
+
+        for (int i = 0; i < addList.size(); i++) {
+            GraphAddBean graphAddBean = addList.get(i);
+            addDateList.add(graphAddBean.getDate());
+            addConfirmList.add(graphAddBean.getAddConfirm());
+            addSuspectList.add(graphAddBean.getAddSuspect());
+        }
+
+        model.addAttribute("addDateList", new Gson().toJson(addDateList));
+        model.addAttribute("addConfirmList", new Gson().toJson(addConfirmList));
+        model.addAttribute("addSuspectList", new Gson().toJson(addSuspectList));
+
+
+        List<GraphPieBean> pieList = GraphHandler.getGraphPieData(str);
+        Collections.sort(pieList);
+        model.addAttribute("pieList", new Gson().toJson(pieList));
+
+        List<GraphColumnarBean> columnarList = GraphHandler.getGraphColumnarData();
+        Collections.sort(columnarList);
+
+        ArrayList<String> nameList = new ArrayList<>();
+        ArrayList<Integer> fromAbroadList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            GraphColumnarBean bean = columnarList.get(i);
+            nameList.add(bean.getArea());
+            fromAbroadList.add(bean.getFromAbroad());
+        }
+
+
+        model.addAttribute("nameList", new Gson().toJson(nameList));
+        model.addAttribute("fromAbroadList", new Gson().toJson(fromAbroadList));
+        System.out.println("我是"+id);
+        return "provincelist";
     }
 }
